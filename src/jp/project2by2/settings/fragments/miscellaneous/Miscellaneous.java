5189711c4c4ec058cdf72d461a0e7489dd6b31d8
kailua <kailua@project2by2.jp>
2026-03-10 17:54:04 +0900

    2byTricks: Adapt to current implementation
    * Also import some strings

--- src/jp/project2by2/settings/fragments/miscellaneous/Miscellaneous.java
+++ src/jp/project2by2/settings/fragments/miscellaneous/Miscellaneous.java
@@ -6,25 +6,14 @@
 package jp.project2by2.settings.fragments.miscellaneous;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.DownloadManager;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.SystemProperties;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
@@ -32,130 +21,49 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
-import org.json.JSONObject;
+import jp.project2by2.settings.preferences.KeyboxDataPreference;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 import java.util.List;
 
 @SearchIndexable
-public class Miscellaneous extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+public class Miscellaneous extends SettingsPreferenceFragment {
 
     private static final String TAG = "Miscellaneous";
+    private static final String KEY_KEYBOX_DATA_SETTING = "keybox_data_setting";
 
-    private static final String KEY_PIF_JSON_MANAGE_PREFERENCE = "pif_json_manage_preference";
-
-    private Preference mPifJsonManagePreference;
-
-    private Handler mHandler;
-
-    private Context context;
-    private Resources resources;
-    private ContentResolver resolver;
+    private KeyboxDataPreference mKeyboxDataPreference;
+    private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.custom_settings_miscellaneous);
 
-        context = getContext();
-        resources = context.getResources();
-        resolver = context.getContentResolver();
+        mKeyboxDataPreference = findPreference(KEY_KEYBOX_DATA_SETTING);
 
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-
-        mHandler = new Handler();
-
-        mPifJsonManagePreference = findPreference(KEY_PIF_JSON_MANAGE_PREFERENCE);
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mPifJsonManagePreference) {
-            onPifManageScreen(context);
-            return true;
-        }
-        return super.onPreferenceTreeClick(preference);
-    }
-
-    private void onPifManageScreen(Context context) {
-        final String[] items = {
-            resources.getString(R.string.spoofing_pif_json_download_title),
-            resources.getString(R.string.spoofing_pif_json_select_title),
-        };
-        new AlertDialog.Builder(context)
-            .setTitle(resources.getString(R.string.spoofing_pif_manage_title))
-            .setItems(items, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    if (which == 0) {
-                        downloadPifJson(context);
-                    } else if (which == 1) {
-                        selectPifJson(context);
+        mKeyboxFilePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    if (result.getResultCode() != Activity.RESULT_OK || result.getData() == null) {
+                        return;
                     }
-                }
-            })
-            .show();
-    }
-
-    private void downloadPifJson(Context context) {
-        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(resources.getString(R.string.spoofing_pif_json_download_url));
-        String mes = resources.getString(R.string.spoofing_pif_json_download_message);
-
-        DownloadManager.Request request = new DownloadManager.Request(uri)
-            .setTitle("pif.json")
-            .setDescription(mes)
-            .setMimeType("application/json")
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "pif.json");
-        downloadManager.enqueue(request);
-
-        Toast.makeText(context, resources.getString(R.string.spoofing_pif_json_download_message), Toast.LENGTH_LONG).show();
-    }
-
-    private void selectPifJson(Context context) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("application/json");
-        startActivityForResult(intent, 10001);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 10001 && resultCode == Activity.RESULT_OK) {
-            Uri uri = data.getData();
-            Log.d(TAG, "URI received: " + uri.toString());
-            try (InputStream inputStream = getActivity().getContentResolver().openInputStream(uri)) {
-                if (inputStream != null) {
-                    String json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-                    Log.d(TAG, "JSON data: " + json);
-                    JSONObject jsonObject = new JSONObject(json);
-                    for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
-                        String key = it.next();
-                        String value = jsonObject.getString(key);
-                        Log.d(TAG, "Setting property: persist.sys.pihooks_" + key + " = " + value);
-                        SystemProperties.set("persist.sys.pihooks_" + key, value);
+                    final Uri uri = result.getData().getData();
+                    if (uri == null || mKeyboxDataPreference == null) {
+                        return;
                     }
-                    Toast.makeText(
-                        getContext(),
-                        getContext().getResources().getString(R.string.spoofing_pif_json_select_success),
-                        Toast.LENGTH_LONG
-                    ).show();
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Error reading JSON or setting properties", e);
-            }
-        }
-    }
+                    try {
+                        final int takeFlags = result.getData().getFlags()
+                                & Intent.FLAG_GRANT_READ_URI_PERMISSION;
+                        requireContext().getContentResolver()
+                                .takePersistableUriPermission(uri, takeFlags);
+                    } catch (SecurityException e) {
+                        Log.w(TAG, "Failed to persist uri permission for keybox file", e);
+                    }
+                    mKeyboxDataPreference.handleFileSelected(uri);
+                });
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
-        return false;
+        if (mKeyboxDataPreference != null) {
+            mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
+        }
     }
 
     @Override
@@ -169,7 +77,6 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
             @Override
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
-                final Resources resources = context.getResources();
                 return keys;
             }
         };
